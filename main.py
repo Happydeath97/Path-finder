f92316cffbb7827bc363b3ccd76462e49ccb43e6
Happydeath97 <98758077+Happydeath97@users.noreply.github.com>
2022-03-29 20:57:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 import pygame
-from herni_plan import *
 import queue
 
 WIDTH = 750
@@ -8,66 +7,84 @@ ROWS = 50
 TILESIZE = WIDTH // ROWS
 BLACK = (0, 0, 0)
 CLOCK = pygame.time.Clock()
-MAPA = WORLD_MAP
 pygame.display.set_caption("PATH FINDER")
 
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 
+
 def find_path(mapa):
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
-        start = "O"
-        end = "X"
-        start_pos = find_start(mapa, start)
-        q = queue.Queue()
-        q.put((start_pos, [start_pos]))
-        visited = set()
+    start = "O"
+    end = "X"
+    start_pos = find_start(mapa, start)
+    end_pos = find_end(mapa, end)
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
+    visited = set()
 
-        while not q.empty():
-            current_pos, path = q.get()
-            row, col = current_pos
-            i = path[-1]
-            r, c = i
-            pygame.draw.rect(WIN, (61, 90, 254), (c*TILESIZE, r*TILESIZE, TILESIZE, TILESIZE))
-            pygame.display.update()
-            time.sleep(0.005)
-            if mapa[row][col] == end:
-                for i in path:
-                    r, c = i
-                    pygame.draw.rect(WIN, (144, 202, 249), (c*TILESIZE, r*TILESIZE, TILESIZE, TILESIZE))
-                    pygame.display.update()
-                while True:
-                    pygame.init()
-                    event = pygame.event.wait()
-                    if event.type == pygame.KEYDOWN:
-                        pygame.quit()
-                        exit()
+    if end_pos == None:
+        draw_win()
 
-            neighbors = find_neighbors(mapa, row, col)
-            for neighbor in neighbors:
-                if neighbor in visited:
-                    continue
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
+        i = path[-1]
+        r, c = i
+        pygame.draw.rect(WIN, (61, 90, 254), (c * TILESIZE, r * TILESIZE, TILESIZE, TILESIZE))
+        pygame.display.update()
+        time.sleep(0.005)
+        if mapa[row][col] == end:
+            for i in path:
+                r, c = i
+                pygame.draw.rect(WIN, (144, 202, 249), (c * TILESIZE, r * TILESIZE, TILESIZE, TILESIZE))
+                pygame.display.update()
 
-                r, c = neighbor
-                if mapa[r][c] == "x":
-                    continue
+            while True:
+                pygame.init()
+                event = pygame.event.wait()
+                if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                    if not pygame.key.get_pressed()[pygame.K_SPACE]:
+                        return
 
-                new_path = path + [neighbor]
-                q.put((neighbor, new_path))
-                visited.add(neighbor)
+        neighbors = find_neighbors(mapa, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if mapa[r][c] == "x":
+                continue
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+            
+    if end_pos not in visited:
+        draw_win()
+
 
 def find_neighbors(mapa, row, col):
     neighbors = []
 
-    if row > 0:     # up
+    if row > 0:  # up
         neighbors.append((row - 1, col))
-    if row + 1 < len(mapa):     # down
+    if row + 1 < len(mapa):  # down
         neighbors.append((row + 1, col))
-    if col > 0:     # left
+    if col > 0:  # left
         neighbors.append((row, col - 1))
-    if col + 1 < len(mapa[0]):      # right
+    if col + 1 < len(mapa[0]):  # right
         neighbors.append((row, col + 1))
 
     return neighbors
+
+
+def find_end(mapa, end):
+    for r, row in enumerate(mapa):
+        for c, value in enumerate(row):
+            if value == end:
+                return r, c
+
+    return None
+
 
 def find_start(mapa, start):
     for r, row in enumerate(mapa):
@@ -77,39 +94,41 @@ def find_start(mapa, start):
 
     return None
 
-def turn1():
+
+def turn1(mapa):
     if pygame.mouse.get_pressed()[0]:
         x = pygame.mouse.get_pos()[0]
         y = pygame.mouse.get_pos()[1]
         row = y // TILESIZE
         column = x // TILESIZE
-        if MAPA[row][column] == "x":
-            MAPA[row][column] = "#"
+        if mapa[row][column] == "x":
+            mapa[row][column] = "#"
 
     if pygame.key.get_pressed()[pygame.K_v]:
         x = pygame.mouse.get_pos()[0]
         y = pygame.mouse.get_pos()[1]
         row = y // TILESIZE
         column = x // TILESIZE
-        if MAPA[row][column] == "x":
-            MAPA[row][column] = "O"
+        if mapa[row][column] == "x":
+            mapa[row][column] = "O"
 
     if pygame.key.get_pressed()[pygame.K_c]:
         x = pygame.mouse.get_pos()[0]
         y = pygame.mouse.get_pos()[1]
         row = y // TILESIZE
         column = x // TILESIZE
-        if MAPA[row][column] == "x":
-            MAPA[row][column] = "X"
+        if mapa[row][column] == "x":
+            mapa[row][column] = "X"
 
     if pygame.mouse.get_pressed()[2]:
         x = pygame.mouse.get_pos()[0]
         y = pygame.mouse.get_pos()[1]
         row = y // TILESIZE
         column = x // TILESIZE
-        MAPA[row][column] = "x"
+        mapa[row][column] = "x"
 
-def draw_win():
+
+def draw_win(mapa):
     WIN.fill(BLACK)
     size_btwn = WIDTH // ROWS
     x = 0
@@ -121,29 +140,36 @@ def draw_win():
         pygame.draw.line(WIN, (255, 255, 255), (x, 0), (x, WIDTH))
         pygame.draw.line(WIN, (255, 255, 255), (0, y), (WIDTH, y))
 
-    for row_index, row in enumerate(MAPA):
+    for row_index, row in enumerate(mapa):
         for col_index, col in enumerate(row):
             x = col_index * TILESIZE
             y = row_index * TILESIZE
-            if col == "#":      # Path
+            if col == "#":  # Path
                 pygame.draw.rect(WIN, (160, 100, 80), (x, y, TILESIZE, TILESIZE))
-            if col == "x":      # Wall
-                pygame.draw.rect(WIN, (110, 110, 110), (x+1, y+1, TILESIZE-2, TILESIZE-2))
-            if col == "O":      # Start
+            if col == "x":  # Wall
+                pygame.draw.rect(WIN, (110, 110, 110), (x + 1, y + 1, TILESIZE - 2, TILESIZE - 2))
+            if col == "O":  # Start
                 pygame.draw.rect(WIN, (0, 160, 0), (x, y, TILESIZE, TILESIZE))
-            if col == "X":      # Cil
+            if col == "X":  # Cil
                 pygame.draw.rect(WIN, (160, 0, 0), (x, y, TILESIZE, TILESIZE))
 
     pygame.display.update()
 
+
 def main():
+    mapa = [["x" for i in range(50)] for i in range(50)]
     flag = True
     while flag:
         pygame.time.delay(0)
         CLOCK.tick(60)
-        draw_win()
-        turn1()
-        find_path(MAPA)
+        draw_win(mapa)
+        turn1(mapa)
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            try:
+                find_path(mapa)
+                mapa = [["x" for i in range(50)] for i in range(50)]
+            except TypeError:
+                pass
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 flag = False
